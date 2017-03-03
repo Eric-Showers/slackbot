dc544a7f7b814b11dcffde7b8ebf8c0f3ddc6edf
E. Showers <E. Showers>
2017-03-03 13:36:21 -0800

    added word to emoji

--- index.js
+++ index.js
@@ -18,24 +18,51 @@ controller.spawn({
     //token: process.env.SLACK_API_TOKEN
 }).startRTM();
 
+//Do stuff with bot
+
 const createReaction = (message, emoji) => ({
     timestamp: message.ts,
     channel: message.channel,
     name: emoji
 });
 
+//Respond to greetings
 controller.hears('hello',  ['direct_message', 'direct_mention'], 
     (bot, message) => {
         bot.reply(message, 'Hey there 👋');
     }
 );
 
+//Taunt Dan
 controller.hears('by Daniel Frankcom', ['direct_message','ambient'], (bot, message) => {
     bot.api.reactions.add(createReaction(message, 'nerd_face'));
 	}
 );
 
+//Initiate poll
 controller.hears('vote!', ['ambient'], (bot, message) => {
     bot.api.reactions.add(createReaction(message, 'thumbsup'));
     bot.api.reactions.add(createReaction(message, 'thumbsdown'));
+});
+
+controller.hears('.+', ['mention', 'direct_mention', 'direct_message'], (bot, message) =>  {
+    // Get text from message
+    const text = message.text.toLowerCase().trim();
+
+    // split message on [space] character
+    const words = text.split(' ');
+
+    // Loop through all the words
+    words.forEach((word) => {
+        word = word.trim();
+
+        // Try and match the word to an emoji
+        const emoji = emojiFromWord(word);
+
+        // If an emoji was found with confidence > 93%
+        //  add the emoji as a reaction
+        if (emoji.emoji_name && emoji.score > 0.93) {
+            bot.api.reactions.add(createReaction(message, emoji.emoji_name));
+        }
+    });
 });
